28d94154ef4cd44e076fe769cf31fbfdb765256a
HHT <harashtatandjung@gmail.com>
2020-02-10 23:09:01 +0700

    &*

--- Goj.py
+++ Goj.py
@@ -32,3 +32,45 @@
 # print(foo(arr1, 6, 7, 8, 8))
 
 # No 3
+
+# string = "fruits"
+
+# print(string[1])
+
+def g(str):
+    i = 0
+    new_str = ""
+    while i<(len(str)-1):
+        new_str = new_str+str[i+1]
+        i += 1
+    return new_str
+
+def f(str):
+    if len(str) == 0:
+        return ""
+    elif len(str) == 1:
+        return str
+    else:
+        return f(g(str)) + str[0]
+
+def h(int_n, str_string):
+    while int_n != 1:
+        if int_n %2 == 0:
+            int_n = int_n/2
+        else:
+            int_n = (3*int_n) +1
+        str_string = f(str_string)
+    return str_string
+
+def powe(int_x, int_y):
+    if int_y == 0:
+        return 1
+    else:
+        int_x = int_x * powe(int_x, int_y-1) 
+        
+
+print(h(1,"fruits"))
+print(h(2,"fruits"))
+print(h(5,"fruits"))
+print(h(powe(2, 1000000000000000), "fruits"))
+print(h(powe(2, 9831050005000007), "fruits"))
