75ad4a1de81d834fbf4efae1095f42253e3e90ca
HHT <harashtatandjung@gmail.com>
2020-02-10 22:14:45 +0700

    test`

---
+++ Goj.py
@@ -0,0 +1,34 @@
+# No 1
+# 
+# # arr = [5,7,8,3]
+
+# i = 0
+# s = 0
+
+# while i<len(arr):
+#     s = s+arr[i]
+#     i = i+1
+#     print('i',i, 's', s)
+
+# No 2
+
+# arr1 = [11,10,10,5,10,15,20,10,7,11]
+
+# def foo(arr1, int_a, int_b, int_i, int_j):
+#     k = int_j
+#     ct = 0
+#     while k > int_i-1:
+#         if (arr1[k] <= int_b) and not (arr1[k] <= int_a):
+#             ct += 1
+#             break
+#         k = k-1
+#         break
+#     return ct
+
+# print(foo(arr1, 8, 18, 3, 6))
+# print(foo(arr1, 10, 20, 0, 9))
+# print(foo(arr1, 8, 18, 6, 3))
+# print(foo(arr1, 20, 10, 0, 9))
+# print(foo(arr1, 6, 7, 8, 8))
+
+# No 3
