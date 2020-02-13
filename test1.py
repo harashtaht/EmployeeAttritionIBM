e13f9da4c3eb1e0765240bafb1d555cbc63a77e2
HHT <harashtatandjung@gmail.com>
2020-02-13 22:51:21 +0700

    a

--- test1.py
+++ test1.py
@@ -1,28 +1,33 @@
 # No 4
 
-test_A = input()
-num_T = int(test_A)
-
 def divisible(dictNum):
     results = 0
     for i in range(dictNum["A"], dictNum["B"]+1):
         if i%dictNum["K"] == 0:
             results+=1
         else:
-            continue
+            results+=0
     return results
 
+test_A = input()
+num_T = int(test_A)
 
-dictOutput = {}
-for i in range(1, num_T+1):
-    dictNum = {}
-    dictNum["A"] = int(input())
-    dictNum["B"] = int(input())
-    dictNum["K"] = int(input())
-    dictOutput[i] = divisible(dictNum)
+if (1<=num_T<=100):
+    dictOutput = {}
+    for i in range(1, num_T+1):
+        dictNum = {}
+        dictNum["A"] = int(input())
+        dictNum["B"] = int(input())
+        dictNum["K"] = int(input())
+        dictOutput[i] = divisible(dictNum)
+else:
+    breakpoint
+
+    # print(dictNum)
     # print(f'Case {i}: {divisible(dictNum)}')
 # print(dictOutput)
 # dictOutput = {1: 3, 2: 4}
 
-for i in range(1, len(dictOutput)+1):
-    print(f'Case {i}: {dictOutput[i]}')
+# for i in range(1, len(dictOutput)+1):
+#     print(f'Case {i}: {dictOutput[i]}')
+
