4d41d22394af263f6c9b0aa1d1e2b1af69c831c5
HHT <harashtatandjung@gmail.com>
2020-02-13 21:39:14 +0700

    No 4

--- Goj.py
+++ Goj.py
@@ -37,40 +37,67 @@
 
 # print(string[1])
 
-def g(str):
-    i = 0
-    new_str = ""
-    while i<(len(str)-1):
-        new_str = new_str+str[i+1]
-        i += 1
-    return new_str
+# def g(str):
+#     i = 0
+#     new_str = ""
+#     while i<(len(str)-1):
+#         new_str = new_str+str[i+1]
+#         i += 1
+#     return new_str
 
-def f(str):
-    if len(str) == 0:
-        return ""
-    elif len(str) == 1:
-        return str
-    else:
-        return f(g(str)) + str[0]
+# def f(str):
+#     if len(str) == 0:
+#         return ""
+#     elif len(str) == 1:
+#         return str
+#     else:
+#         return f(g(str)) + str[0]
 
-def h(int_n, str_string):
-    while int_n != 1:
-        if int_n %2 == 0:
-            int_n = int_n/2
-        else:
-            int_n = (3*int_n) +1
-        str_string = f(str_string)
-    return str_string
+# def h(int_n, str_string):
+#     while int_n != 1:
+#         if int_n %2 == 0:
+#             int_n = int_n/2
+#         else:
+#             int_n = (3*int_n) +1
+#         str_string = f(str_string)
+#     return str_string
 
-def powe(int_x, int_y):
-    if int_y == 0:
-        return 1
-    else:
-        int_x = int_x * powe(int_x, int_y-1) 
+# def powe(int_x, int_y):
+#     if int_y == 0:
+#         return 1
+#     else:
+#         int_x = int_x * powe(int_x, int_y-1) 
         
 
-print(h(1,"fruits"))
-print(h(2,"fruits"))
-print(h(5,"fruits"))
-print(h(powe(2, 1000000000000000), "fruits"))
-print(h(powe(2, 9831050005000007), "fruits"))
+# print(h(1,"fruits"))
+# print(h(2,"fruits"))
+# print(h(5,"fruits"))
+# print(h(powe(2, 1000000000000000), "fruits"))
+# print(h(powe(2, 9831050005000007), "fruits"))
+
+## No 4
+
+# A, B, K
+# Each test case has three numbers A, B, K
+
+# Number of test cases T -> First line
+# Output: Case C: X
+# Where C is the case number (starting from 1)
+# X is how many number divisible by K between A and B
+
+# Constraints:
+# 1<= T <= 100
+# 1<= A <=B <10000
+# 1<= K < 10000
+
+#Sample Input
+# 2 -> Number of test cases T (2 Times test)
+# 1 -> A
+# 10 -> B
+# 3 -> K
+# 8 -> A
+# 20 -> B
+# 4 -> K
+
+num_T = int(input())
+# print(num_T)
